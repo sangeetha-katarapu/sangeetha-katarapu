688533a1817450672d84a945607c3430cbc0e7f3
Sangeetha <ksangeetha@quadratyx.com>
2021-07-17 01:19:02 +0530

    edited navbar

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuardService } from './services/auth-guard.service'
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 const routes: Routes = [
-  
+  { path: 'hhhhhhome', component: HomeComponent, canActivate: [AuthGuardService], data: { roles: ['admin', 'user'] } },
+
+  { path: 'home', component: HomeComponent },
   { path: '**', component: LoginComponent },
+
 ];
 
 @NgModule({
